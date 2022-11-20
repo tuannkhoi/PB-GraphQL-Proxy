d0c373342d2d54fbe80a2f61921fcbf2678b343f
khoi.tuan <khoi.tuan@ladbrokes.com.au>
2022-11-20 22:17:06 +1000

    feat: add CORS

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	log "github.com/sirupsen/logrus"
@@ -36,6 +37,8 @@ func playgroundHandler() gin.HandlerFunc {
 func main() {
 	router := gin.Default()
 
+	router.Use(CORSMiddleware())
+
 	router.GET("/", playgroundHandler())
 	router.POST("/query", graphqlHandler())
 
@@ -49,3 +52,20 @@ func main() {
 		log.Fatalf("got error running GraphQL Proxy: %v", err)
 	}
 }
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+
+		// handle CORS preflight requests
+		// https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		} else {
+			c.Next()
+		}
+	}
+}
+
